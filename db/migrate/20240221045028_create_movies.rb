6551d011789a9f03d005d2626f02faa439a5f589
MACKENZIE_HAMLETT <mackenzie.s.hamlett@gmail.com>
2024-02-21 01:19:40 -0600

    second comits

--- db/migrate/20240221045028_create_movies.rb
+++ db/migrate/20240221045028_create_movies.rb
@@ -1,16 +1,16 @@
 class CreateMovies < ActiveRecord::Migration[7.0]
-    def change
-          create_table 'movies' do |t|
-                  t.string 'title'
-                        t.string 'rating'
-                              t.text 'description'
-                                    t.datetime 'release_date'
-                                    # Add fields that let Rails automatically keep track
-                                    # of when movies are added or modified:
-                                    t.timestamps
-                                  end
-                                end
-                              end
+  def change
+    create_table 'movies' do |t|
+      t.string 'title'
+      t.string 'rating'
+      t.text 'description'
+      t.datetime 'release_date'
+      # Add fields that let Rails automatically keep track
+      # of when movies are added or modified:
+      t.timestamps
+    end
+  end
+end
 
 
 
